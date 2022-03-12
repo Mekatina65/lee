884491bf11870c009256988ba5cbe318c3931c61
squiggs <brennan@bell.net>
2022-03-12 15:31:13 -0500

    update all

--- pages/about.js
+++ pages/about.js
@@ -6,12 +6,12 @@ const About
     return (  
             
         <div>
-        <Navbar />
+       
         <h1>this is about</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.   <br /> 
             In, doloribus asperiores quasi laborum praesentium autem facilis, nisi dolores fuga accusantium <br /> 
             temporibus laboriosam dolorum neque vel eligendi velit adipisci iste ipsa?</p>
-            <Footer />
+    
         
       
        
--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,13 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
+   
             <h1>this is home page  or index.js</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, reprehenderit impedit, 
       dolor molestias rerum deleniti pariatur consectetur accusantium nemo nihil laudantium cupiditate soluta exercitationem <br />
        quasi magnam, incidunt porro? Atque, tenetur?</p>
        <Link href="/ninjas"><a>see ninjas list</a></Link> 
-       <Footer />
+     
     </div>
   )
 }
